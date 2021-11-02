395134dbfa34a70b7c3bbd03ade1b217dc11f1ae
SatwikaKacham <73744169+SatwikaKacham@users.noreply.github.com>
2021-11-02 17:00:19 +0530

    docs

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 var btn = document.querySelectorAll(".btn");
+var enterBtn = document.querySelector(".enter");
 var inputData = document.querySelector(".data");
+var outPut = document.querySelector(".output");
+
+console.log(enterBtn);
+function inputTaker(){
+    console.log( inputData.value);
+    var msg=inputData.value;
+    outPut.innerText="Please fill the require Data 🥺";
+
+}
+function fontSmaller()
+{
+    
+}
+
+
+
+
+
+
+enterBtn.addEventListener('click',inputTaker);
+btn[1].addEventListener('click',fontSmaller);
+btn[0].addEventListener('click',fontBigger);
